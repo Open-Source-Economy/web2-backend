3c7d588864477b840e363812a8bda0db3be9aeae
Lauriane Mollier <lauriane@open-source-economy.com>
2025-03-07 10:17:06 +0100

    Add dto GetSponsor

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "../src/createApp";
-import { config, logger } from "../src/config";
-
-const app = createApp();
-
-app.listen(config.port, () => {
-  logger.info(`Running on Port ${config.port}`);
-});
